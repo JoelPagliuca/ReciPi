a63db983132896613c9499bb5a3c6d67387f0b4c
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2019-06-18 21:05:19 +1000

    don't assume output format

--- Deployment/init-env.sh
+++ Deployment/init-env.sh
@@ -6,7 +6,7 @@ if [ -f Secrets/db.env ]; then
 	exit 0
 fi
 
-DB_PASSWORD=$(aws secretsmanager get-random-password --exclude-punctuation --password-length=30 | jq -r '.RandomPassword')
+DB_PASSWORD=$(aws secretsmanager get-random-password --exclude-punctuation --password-length=30 --output=json | jq -r '.RandomPassword')
 echo "POSTGRES_PASSWORD=${DB_PASSWORD}" > Secrets/db.env
 
 dotnet user-secrets set "DB:ConnectionString" "host=127.0.0.1;port=5432;database=postgres;username=postgres;password=${DB_PASSWORD}"
