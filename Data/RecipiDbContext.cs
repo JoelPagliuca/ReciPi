efec8965383f32cdd80389a3d05a1e390bf0da63
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2019-06-18 22:50:50 +1000

    add migration

--- Data/RecipiDbContext.cs
+++ Data/RecipiDbContext.cs
@@ -8,9 +8,18 @@ namespace recipi.Data
 	{
 		public RecipiDbContext(DbContextOptions<RecipiDbContext> options) : base(options) {}
 		public DbSet<Recipe> Recipes { get; set; }
+		public DbSet<Step> Steps { get; set; }
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);
+			
+			modelBuilder.Entity<Step>()
+				.HasKey(s => new { s.Number, s.RecipeId });
+			modelBuilder.Entity<Step>()
+				.HasOne(s => s.Recipe)
+				.WithMany(r => r.Steps)
+				.OnDelete(DeleteBehavior.Cascade);
+
 			// https://medium.com/front-end-weekly/net-core-web-api-with-docker-compose-postgresql-and-ef-core-21f47351224f ?
 		}
 	}
---
+++ Migrations/20190618122845_Step.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace recipi.Migrations
+{
+    public partial class Step : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Difficulty",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Serves",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeCook",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeOther",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimePrep",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Steps",
+                columns: table => new
+                {
+                    Number = table.Column<int>(nullable: false),
+                    RecipeId = table.Column<int>(nullable: false),
+                    Ingredient = table.Column<string>(nullable: true),
+                    Unit = table.Column<string>(nullable: true),
+                    Amount = table.Column<float>(nullable: false),
+                    Instruction = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Steps", x => new { x.Number, x.RecipeId });
+                    table.ForeignKey(
+                        name: "FK_Steps_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steps_RecipeId",
+                table: "Steps",
+                column: "RecipeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Steps");
+
+            migrationBuilder.DropColumn(
+                name: "Difficulty",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Serves",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "TimeCook",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "TimeOther",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "TimePrep",
+                table: "Recipes");
+        }
+    }
+}
